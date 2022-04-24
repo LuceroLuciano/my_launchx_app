05e7ac477a0226a3345bf25c8a93fd33d95f974f
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-24 11:06:54 -0500

    Paso 8: Modificando la descripcion de la prueba

--- test/missionComander.test.js
+++ test/missionComander.test.js
@@ -1,8 +1,10 @@
 // Prueba de unidad de la clase MissionComander
 
-describe("Esto es una suite de pruebas", () => {
-    test('Caso de prueba 1', () => {
-        const result = 1 + 2
-        expect(result).toBe(3); // Sustituir  3 por 10 o cualquier otro numero para que la prueba falle 
+const MissionComander = require('./../app/missionComander');
+
+describe("Unit Test for Mission Comander Class", () => {
+    test('Created a mission commander object', () => {
+        const myMissionCommander = new MissionComander("Woopa")
+        expect(myMissionCommander.name).toBe("Woopa"); 
     });
 })
